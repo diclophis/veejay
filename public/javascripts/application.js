59fd49d55fb2085d114d8c5f99c481f616d93140
root <diclophis@gmail.com>
2008-12-05 15:15:27 +0000

    working multi source

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,7 @@
 // This file is automatically included by javascript_include_tag :defaults
 var sc;
 var list;
+var youtube_remote_id;
 
 play_video = function (remote_id) {
   chunks = remote_id.split("-");
@@ -20,21 +21,50 @@ play_video = function (remote_id) {
       flashvars.eID = '1301797';
       flashvars.ympsc = '4195351';
       flashvars.lang = 'en';
-      flashvars.shareEnabled = '1';
-      flashvars.enableFullScreen = 'true';
-      flashvars.autoStart = '0';
+      flashvars.shareEnabled = '0';
+      flashvars.enableFullScreen = '0';
+      flashvars.controlsEnable = '0';
+      flashvars.autoStart = '1';
       flashvars.eh = 'yahoo_event_handler';
       swfobject.embedSWF("http://d.yimg.com/cosmos.bcst.yahoo.com/up/fop/embedflv/swf/fop.swf", "player", "512", "332", "9.0.0", false, flashvars, params, attributes);
     break;
     case "mtv":
       var flashvars = {};
+      flashvars.autoPlay = 'true';
       swfobject.embedSWF("http://media.mtvnservices.com/mgid:uma:video:api.mtvnservices.com:" + remote_id, "player", "512", "332", "9.0.0", false, flashvars, params, attributes);
     break;
     case "youtube":
       var flashvars = {};
-      swfobject.embedSWF("http://www.youtube.com/v/" + remote_id + "&enablejsapi=1&playerapiid=ytplayer", "player", "512", "332", "9.0.0", false, flashvars, params, attributes);
+      //swfobject.embedSWF("http://www.youtube.com/v/" + remote_id + "&enablejsapi=1&playerapiid=the_player", "player", "512", "332", "9.0.0", false, flashvars, params, attributes);
+      youtube_remote_id = remote_id;
+      swfobject.embedSWF("http://www.youtube.com/apiplayer?enablejsapi=1&playerapiid=the_player", "player", "512", "332", "9.0.0", false, flashvars, params, attributes);
     break;
   }
+}
+
+function youtube_state_change (state) {
+  //Fired whenever the player's state changes.
+  //Possible values are:
+  //unstarted (-1), 
+  //ended (0), 
+  //playing (1), 
+  //paused (2), 
+  //buffering (3), 
+  //video cued (5). 
+  //When the SWF is first loaded, it will broadcast an unstarted (-1) event.
+  //When the video is cued and ready to play, it will broadcast a video cued event (5).
+  //alert("Player's new state: " + state);
+}
+
+function youtube_error () {
+  //alert("error");
+}
+
+function onYouTubePlayerReady (player_id) {
+  player = swfobject.getObjectById(player_id);
+  player.addEventListener("onStateChange", "youtube_state_change");
+  player.addEventListener("onError", "youtube_error");
+  player.loadVideoById(youtube_remote_id, 0);
 }
 
 mtv_state_change = function (state) {
@@ -63,7 +93,7 @@ function mtvnSetCoad(adObject) {
 }
 
 yahoo_event_handler = function( pType, pItem ) {
-  alert(pType);
+  //alert(pType);
   switch (pType){
     case "init":            // thrown when the player has been initialized and the flash external API is ready to be accessed
     break;
@@ -152,7 +182,7 @@ Event.observe(window, 'load', function () {
     });
     */
     //play_video(videos.first());
-    play_video(videos[1]);
+    play_video(videos[0]);
   }
   /*
   if ($('uvp_fop_container')) {
